b4440d790c754558eb401b49c460b6cf09444212
Liya Zhunussova <lichka10@mail.ru>
2021-06-21 14:48:40 +0300

    Spark Sql

--- mySpark/src/main/java/ru/mai/dep508/bigdata/spark/Sql.java
+++ mySpark/src/main/java/ru/mai/dep508/bigdata/spark/Sql.java
@@ -1,26 +1,27 @@
 package ru.mai.dep508.bigdata.spark;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.SaveMode;
-import org.apache.spark.sql.DataFrame;
-import java.util.Properties;
-import java.util.concurrent.TimeUnit ;
 
+import scala.Tuple2;
+/**
+
+
+ */
 public class Sql {
-
-    private static final String[] POST_FIELDS = new String[]{
-             "CreationDate", "ViewCount", "Score", "AnswerCount", "Tags","PostTypeId"
-    };
 
     public static void main(String[] args) throws Exception {
 
         final long then = System.nanoTime();
-        SparkConf sparkConf = new SparkConf().setAppName("Spark Post Sql");
+        SparkConf sparkConf = new SparkConf().setAppName("Spark Post Test");
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
         SQLContext sqlContext = new SQLContext(sparkContext);
 
@@ -30,42 +31,96 @@ public class Sql {
 
 
         // Чтение пользователей из XML файла и преобразование в объекты Row
-        JavaRDD<Row> postRdd = sparkContext.textFile(postPath).map(post -> XmlUtils.parseXmlToRow(post, POST_FIELDS));
+        JavaPairRDD<String, Tuple2<Integer, Double> > posts = sparkContext
+            .textFile(postPath)
+            .map(post -> XmlUtils.parseXmlToMap(post))
+            .filter(post -> "1".equals(post.get("PostTypeId")))
+            .filter(post -> Integer.parseInt(post.get("ViewCount")) >= 50000)
+            .flatMapToPair(post -> {
+                int ViewCount=Integer.parseInt(post.get("ViewCount"));
+                int year=Integer.parseInt(post.get("CreationDate").substring(0,4));
+                int Score = Integer.parseInt(post.get("Score"));
+                int AnswerCount = Integer.parseInt(post.get("AnswerCount"));
+                String Tags=post.get("Tags");
+                double result= (double) ViewCount/10000  + (double)Score/100 + (double)AnswerCount/20;
 
-        // Создание датафрейма и временной таблицы POST из RDD.
-        sqlContext.createDataFrame(postRdd, XmlUtils.createDataFrameStruct(POST_FIELDS)).registerTempTable("POST");
+                List<Tuple2<String, Double> > res = new ArrayList<>();
 
-        // SQL Запрос 
-        sqlContext.sql("with filter_post as( select YEAR(CreationDate) as year, " +
-                " ViewCount/10000+Score/100+AnswerCount/20  as result , Tags"+
-                " from POST where(PostTypeId==1 and ViewCount>50000 "+
-                "), tag_split as (select year, result, tag from filter_post"+
-                " LATERAL VIEW explode (split(substr(Tags,5,length(Tags)-8),\"&gt;&lt;\") ) my_Table as tag"+
-                "), sum_by_year_and_tag as (select year, tag , sum(result)  result from tag_split  group by year, tag)"+
-                ", tag2020 as (select tag from sum_by_year_and_tag where year=2020)"+
-                ", filter_by_list as ("+
-                      "select year , s.tag, result from sum_by_year_and_tag as s"+
-                     " inner join tag2020 as t on t.tag=s.tag"+
-                 "),  coef_by_tag as  ("+
-                     "select tag, sum ((year-2008)*(year-2008)) as A,"+ 
-                      "sum (year-2008) as B, sum ((year-2008)*result) as C, count(*) as D, sum(result ) as E"+
-                     " from  filter_by_list group by tag"+
-                     ") tag_trand  as ( "+
-                     " select tag, (C*D-E*B)/(A*D-B*B) as a, (E*A-B*C)/(D*A-B*B) as b"+
-                      "from coef_by_tag where D>1  " +
-                      ") select tag, a*13+b as res from  tag_trand order by res desc limit 10;")
-            
-                .coalesce(2)
-         
-                .write()
-       
-                .format("org.apache.spark.sql.json")
+                String[] tags = Tags.substring(4,Tags.length()-4).split("&gt;&lt;");
+                for(String tag: tags) {
+                    res.add(new Tuple2<>(Integer.toString(year) + tag, result));
+                }
+                
+                return res.iterator();
+            })
+            .aggregateByKey(
+                0D, 
+                (sum, post) -> sum + post,
+                (sum1, sum2) -> sum1 + sum2
+            )
+            .mapToPair(row -> {
+                return new Tuple2<>(
+                    row._1.substring(4), // tag
+                    new Tuple2<>(
+                        Integer.parseInt(row._1.substring(0, 4)), // year
+                        row._2 // double result
+                    )
+                );
+            });
+        
+        JavaPairRDD<String, Tuple2<Integer, Double> > posts2020 = posts
+            .filter(post -> 2020 == post._2._1);
+        
+        posts
+            .join(posts2020)
+            .mapToPair(post -> new Tuple2<>(post._1, post._2._1) ) // <Tag, <Year, Result> >
+            .aggregateByKey(
+                // (A, B, C, D, E)
+                Arrays.asList(0D, 0D, 0D, 0D, 0D),
+                // (new Tuple5<Double, Double, Double, Double, Double>(0D, 0D, 0D, 0D, 0D)),
 
-                .mode(SaveMode.Overwrite)
-   
-                .save(outPath);
+                (coeff, row) -> Arrays.asList(
+                    coeff.get(0) + (row._1-2008) * (row._1 - 2008),
+                    coeff.get(1) + (row._1-2008),
+                    coeff.get(2) + (row._1-2008)*row._2,
+                    coeff.get(3) + 1,
+                    coeff.get(4) + row._2
+                ),
+                
+                (coeff1, coeff2) -> Arrays.asList(
+                    coeff1.get(0) + coeff2.get(0),
+                    coeff1.get(1) + coeff2.get(1),
+                    coeff1.get(2) + coeff2.get(2),
+                    coeff1.get(3) + coeff2.get(3),
+                    coeff1.get(4) + coeff2.get(4)
+                )
+            )
+            .filter(row -> row._2.get(3) > 1)
+            .mapToPair(row -> {
+                double A = row._2.get(2) * row._2.get(3) - row._2.get(1) * row._2.get(4);
+                double B = row._2.get(0) * row._2.get(4) - row._2.get(1) * row._2.get(2);
+                double tmp = row._2.get(0) * row._2.get(3) - row._2.get(1) * row._2.get(1);
+                
+                A /= tmp;
+                B /= tmp;
 
-         final long millis = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - then);
-          System.out.println("Spark Time: " + millis); // = something around 1000.
+                return new Tuple2<> (row._1 ,-A*13-B);//для сортировки я умножила на -1
+            })
+            .orderBy(row->row._2).taken(10)
+             .map (row-> row._1+ Double.toString(row._2))
+
+            // Результат ожидается небольшой, поэтому сливаем все партиции в одну
+            .coalesce(1)
+            // Создаем writer
+            // .write()
+            // Используя формат JSON
+            // .format("org.apache.spark.sql.json")
+            // С перезаписью
+            // .mode(SaveMode.Overwrite)
+            // Сохраняем в файл (терминальное действие - инициирует запуск всей цепочки)
+            .saveAsTextFile(outPath);
+        
+        final long millis = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - then);
+        System.out.println("Spark Time: " + millis); // = something around 1000.
     }
 }
